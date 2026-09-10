a7228789b2fc9b784c6f1e40cff2086cb5fbfc23
n3tr00x <n3tr00x99@gmail.com>
2026-09-10 03:25:01 +0200

    feat: initilize shadcn ui with dark mode

--- app/page.tsx
+++ app/page.tsx
@@ -1,3 +1,9 @@
+import { Button } from '@/components/ui/button';
+
 export default function Home() {
-	return <div>Hello, world!</div>;
+	return (
+		<div className="flex justify-center">
+			<Button>Click me</Button>
+		</div>
+	);
 }
